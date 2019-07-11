e9ef892218db2e709ca1c146d0e237b7de68c518
FunkGG <huangjiyufg@163.com>
2019-07-11 21:53:37 +0800

    Create threeSumClosest.py

---
+++ day1/threeSumClosest.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        ans = float('inf')
+        n = len(nums)
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left = i+1
+            right = n-1
+            while left<right:
+                tmp = nums[i]+nums[left]+nums[right]
+                if tmp ==target:
+                    return target
+                if abs(ans-target) > abs(tmp-target):
+                    ans = tmp
+                if tmp > target:
+                    right -= 1
+                else:
+                    left += 1
+        return ans
