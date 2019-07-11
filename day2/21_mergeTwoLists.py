4ee6df9bffa9b5a7fdcb9e822c8b9cf79168662a
FunkGG <huangjiyufg@163.com>
2019-07-12 00:51:31 +0800

    Create 21_mergeTwoLists.py

---
+++ day2/21_mergeTwoLists.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        out = l3 =ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                l3.next = l1
+                l1 = l1.next
+            else:
+                l3.next = l2
+                l2 = l2.next
+            l3 = l3.next
+        if l1:
+            l3.next = l1
+        if l2:
+            l3.next = l2
+        return out.next
