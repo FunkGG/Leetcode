c72f421cbb131f9c5308b28e85cbd75e00b4539d
FunkGG <huangjiyufg@163.com>
2019-07-18 10:30:52 +0800

    Update 32_longestValidParentheses.py

--- day5/32_longestValidParentheses.py
+++ day5/32_longestValidParentheses.py
@@ -1,1 +1,30 @@
-
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        out = 0
+        d = {'(': 1, ')': -1}
+        lab = 0
+        tmp = 0
+        for i in range(len(s)):
+            lab += d[s[i]]
+            if lab < 0:
+                tmp = 0
+                lab = 0
+            elif lab == 0:
+                tmp += 1
+                out = max(out, tmp)
+            else:
+                tmp += 1
+        d = {'(': -1, ')': 1}
+        lab = 0
+        tmp = 0
+        for i in range(-1, -len(s)-1, -1):
+            lab += d[s[i]]
+            if lab < 0:
+                tmp = 0
+                lab = 0
+            elif lab == 0:
+                tmp += 1
+                out = max(out, tmp)
+            else:
+                tmp += 1
+        return out
