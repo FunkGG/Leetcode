ba710739c86d8059c108129e981ea02e17f561dd
FunkGG <huangjiyufg@163.com>
2019-07-18 12:51:25 +0800

    Create 41_firstMissingPositive.py

---
+++ day5/41_firstMissingPositive.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.sort()
+        if not nums or nums[0] > 1 or nums[-1] < 1:
+            return 1
+        out = nums[-1]+1
+        for i in range(len(nums)-1):
+            if nums[i+1] > 1 and nums[i+1] - nums[i] > 1:
+                num = nums[i] + 1
+                while num < 1:
+                    num += 1
+                return num
+        return out
